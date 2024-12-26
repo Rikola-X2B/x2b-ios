4318ad6beb95a803f4e417da664a8c278ff69366
Andreas Berg <andreas@rikola.ch>
2024-12-26 15:52:03 +0100

    Initial Commit

---
+++ x2b/x2bApp.swift
@@ -0,0 +1,17 @@
+//
+//  x2bApp.swift
+//  x2b
+//
+//  Created by Andreas Berg on 26.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct x2bApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
